b59da5fa7fb02ccaf634f3642cf69484b91b9d73
gvjacob <gvjacob@outlook.com>
2019-12-22 14:33:47 -0800

    GetInTouch:

---
+++ components/GetInTouch/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import cn from 'classnames';
+
+import css from './styles.css';
+
+const GetInTouch = ({ className }) => {
+  return (
+    <div className={cn(className)} data-testid={'GetInTouch'}>
+      <div className={css.title}>Get in touch!</div>
+      <p className={css.paragraph}>
+        Feel free to{' '}
+        <a className={css.cta} href={'mailto:gvjacob@outlook.com'}>
+          shoot me an email
+        </a>{' '}
+        if you want to work with me, learn more about me, or just want to say
+        hi.
+      </p>
+    </div>
+  );
+};
+
+export default GetInTouch;
--- containers/Index/index.js
+++ containers/Index/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head';
 
 import Updates from '../../components/Updates';
 import Experiences from '../../components/Experiences';
+import GetInTouch from '../../components/GetInTouch';
 
 import css from './styles.css';
 
@@ -9,7 +10,7 @@ import css from './styles.css';
  * Base page ('/'), showcasing Gino.
  */
 const Index = ({ className }) => {
-  const stack = [Updates, Experiences];
+  const stack = [Updates, Experiences, GetInTouch];
 
   return (
     <div className={className}>
