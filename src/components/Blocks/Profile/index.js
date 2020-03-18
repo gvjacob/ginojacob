848537aa41fb1123c44213604451a817f9ce7c26
gvjacob <gvjacob@outlook.com>
2020-03-17 23:49:12 -0400

    Profile background

--- src/components/Blocks/Profile/index.js
+++ src/components/Blocks/Profile/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
-import { Column } from '../Templates';
 
-const Profile = () => <Column>Gino Jacob</Column>;
+import { Column } from '../Templates';
+import styles from './styles.module.scss';
+
+const Profile = () => (
+  <Column className={styles.profile}>
+    <h1 className={styles.headline}>Gino Jacob</h1>
+  </Column>
+);
 
 export default Profile;
