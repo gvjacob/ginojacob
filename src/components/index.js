71683c823e855cd32a1b78933fd938151babf69c
gvjacob <gvjacob@outlook.com>
2020-04-04 23:28:48 -0400

    delete blocks

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,1 @@
-export { default as Grid } from './Grid';
 export { default as SEO } from './SEO';
-export * from './Blocks';
--- src/containers/Index/index.js
+++ src/containers/Index/index.js
@@ -1,15 +1,11 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 
-import { SEO, Grid, Profile, Spotify } from '../../components';
+import { SEO } from '../../components';
 import styles from './styles.module.scss';
 
 const Index = () => (
   <div className={styles.index}>
     <SEO title="Developer, Designer, Ballroom Dancer" />
-    <Grid height="100%" flow="column dense" columns={5} rows={3}>
-      <Profile />
-      <Spotify />
-    </Grid>
   </div>
 );
 
