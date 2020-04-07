8d7e261b7f8e46ab1bc0c8e91e99402aed5319f7
gvjacob <gvjacob@outlook.com>
2020-04-06 23:53:41 -0400

    Introduction gsap animation

--- src/components/Image/index.js
+++ src/components/Image/index.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import { safeGet, classNames as cn } from 'peculiarity';
 
 import styles from './styles.module.scss';
 
-const Image = ({ className, image, fit }) => {
+const Image = ({ className, image, fit }, ref) => {
   const { title, url } = safeGet(image, 'title', ['file', 'url']);
 
-  return <img className={cn(styles.image, fit && styles.fit, className)} src={url} alt={title} />;
+  return (
+    <img
+      className={cn(styles.image, fit && styles.fit, className)}
+      src={url}
+      alt={title}
+      ref={ref}
+    />
+  );
 };
 
-export default Image;
+export default forwardRef(Image);
--- src/containers/Introduction/index.js
+++ src/containers/Introduction/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
+import { gsap } from 'gsap';
 
 import { Image } from '../../components';
 import styles from './styles.module.scss';
@@ -17,13 +18,30 @@ const Introduction = () => {
     }
   `);
 
+  const nameRef = useRef(null);
+  const mediaRef = useRef(null);
+  const biographyRef = useRef(null);
+
+  useEffect(() => {
+    const delta = 40;
+    const duration = 1;
+
+    gsap.fromTo(nameRef.current, { opacity: 0, x: -delta }, { duration, opacity: 1, x: 0 });
+    gsap.fromTo(mediaRef.current, { autoAlpha: 0 }, { duration: 0.5, delay: 0.5, autoAlpha: 1 });
+    gsap.fromTo(biographyRef.current, { opacity: 0, x: delta }, { duration, opacity: 1, x: 0 });
+  }, []);
+
   const { name, biographyTease, media } = biography;
 
   return (
     <section className={styles.introduction}>
-      <h1 className={styles.name}>{name}</h1>
-      <Image className={styles.media} image={media} />
-      <p className={styles.biography}>{biographyTease}</p>
+      <h1 id={'name'} className={styles.name} ref={nameRef}>
+        {name}
+      </h1>
+      <Image className={styles.media} image={media} ref={mediaRef} />
+      <p className={styles.biography} ref={biographyRef}>
+        {biographyTease}
+      </p>
     </section>
   );
 };
