e6cb3b7840fd1e48d9bbeff25a55d6bb0ad9269f
gvjacob <gvjacob@outlook.com>
2020-04-10 00:41:23 -0400

    ProjectsArchive animation

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,13 +1,8 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 import styles from './styles.module.scss';
 import '../../styles/index.scss';
 
 const Layout = ({ children }) => <main className={styles.layout}>{children}</main>;
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-};
 
 export default Layout;
--- src/containers/ProjectsArchive/index.js
+++ src/containers/ProjectsArchive/index.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { classNames as cn } from 'peculiarity';
+import { TweenLite } from 'gsap';
+import ScrollMagic from 'scrollmagic';
+import { ScrollMagicPluginGsap } from 'scrollmagic-plugin-gsap';
 
 import { Link } from '../../components';
 import styles from './styles.module.scss';
+
+ScrollMagicPluginGsap(ScrollMagic, TweenLite);
 
 const Project = ({ className, project }) => {
   const { name, link } = project;
@@ -16,15 +21,33 @@ const Project = ({ className, project }) => {
   );
 };
 
-const ProjectsArchive = ({ className, projects }) => (
-  <section className={className}>
-    <h2 className={styles.overline}>Archive</h2>
-    <ul>
-      {projects.map((project, i) => (
-        <Project project={project} key={i} />
-      ))}
-    </ul>
-  </section>
-);
+const ProjectsArchive = ({ className, projects }) => {
+  const ref = useRef();
+
+  useEffect(() => {
+    const controller = new ScrollMagic.Controller();
+
+    const tween = TweenLite.fromTo(`.project`, { autoAlpha: 0 }, { autoAlpha: 1, stagger: 0.2 });
+
+    new ScrollMagic.Scene({
+      triggerElement: ref.current,
+      triggerHook: 'onEnter',
+      offset: 100,
+    })
+      .setTween(tween)
+      .addTo(controller);
+  }, []);
+
+  return (
+    <section className={className} ref={ref}>
+      <h2 className={styles.overline}>Archive</h2>
+      <ul>
+        {projects.map((project, i) => (
+          <Project className={'project'} project={project} key={i} />
+        ))}
+      </ul>
+    </section>
+  );
+};
 
 export default ProjectsArchive;
