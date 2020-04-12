48551dbc8f4a79def104299653412506746f2794
gvjacob <gvjacob@outlook.com>
2020-04-11 23:40:52 -0400

    ThemeContext and css root variables

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,8 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
+import { ThemeContext } from '../../contexts';
 import styles from './styles.module.scss';
+import { varBeige, varPink, varBlack, varWhite } from '../../styles/abstracts/_variables.scss';
 import '../../styles/index.scss';
 
-const Layout = ({ children }) => <main className={styles.layout}>{children}</main>;
+const getThemeConfiguration = isDarkMode =>
+  isDarkMode
+    ? {
+        ['--primary']: varBeige,
+        ['--secondary']: varWhite,
+        ['--tertiary']: varBlack,
+      }
+    : {
+        ['--primary']: varPink,
+        ['--secondary']: varBlack,
+        ['--tertiary']: varWhite,
+      };
+
+const Layout = ({ children }) => {
+  const [isDarkMode, setIsDarkMode] = useState(true);
+
+  useEffect(() => {
+    const themeConfiguration = getThemeConfiguration(isDarkMode);
+
+    Object.entries(themeConfiguration).forEach(([property, value]) =>
+      document.documentElement.style.setProperty(property, value),
+    );
+  }, [isDarkMode]);
+
+  return (
+    <ThemeContext.Provider value={[isDarkMode, setIsDarkMode]}>
+      <main className={styles.layout}>{children}</main>
+    </ThemeContext.Provider>
+  );
+};
 
 export default Layout;
--- src/components/ThemeSwitch/index.js
+++ src/components/ThemeSwitch/index.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { classNames as cn } from 'peculiarity';
 
+import { ThemeContext } from '../../contexts';
 import styles from './styles.module.scss';
 
 const ThemeSwitch = ({ className }) => {
-  return <i className={cn('far fa-moon', styles.themeSwitch, className)} />;
+  const [isDarkMode, setIsDarkMode] = useContext(ThemeContext);
+
+  return (
+    <button className={className} onClick={() => setIsDarkMode(!isDarkMode)}>
+      <i className={cn(`${isDarkMode ? 'fas' : 'far'} fa-moon`, styles.themeSwitch)} />
+    </button>
+  );
 };
 
 export default ThemeSwitch;
