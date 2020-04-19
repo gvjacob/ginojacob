29c23a241a370ae15da37858f54efaf7508e2d2d
gvjacob <gvjacob@outlook.com>
2020-04-19 16:28:06 -0400

    background grid

--- src/containers/Index/index.js
+++ src/containers/Index/index.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
 
-import { SEO, Column } from '../../components';
-import Introduction from '../Introduction';
-import Experiences from '../Experiences';
-import FeaturedProjects from '../FeaturedProjects';
-import ProjectsArchive from '../ProjectsArchive';
+import { SEO, Grid } from '../../components';
 import styles from './styles.module.scss';
 
 const Index = () => {
@@ -48,11 +44,7 @@ const Index = () => {
   return (
     <div className={styles.index}>
       <SEO title="Developer, Designer, Ballroom Dancer" />
-      <Introduction biography={biography} />
-      <Experiences className={styles.padded} experiences={experiences} />
-      <FeaturedProjects className={styles.featuredProjects} projects={featuredProjects} />
-      <ProjectsArchive className={styles.padded} projects={projects} />
-      <Column className={styles.column} />
+      <Grid className={styles.grid} />
     </div>
   );
 };
