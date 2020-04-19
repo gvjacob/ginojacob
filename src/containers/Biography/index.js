5e85026795d923ee27eff4bea6daec43475e9b13
gvjacob <gvjacob@outlook.com>
2020-04-19 18:24:53 -0400

    organization link

--- src/containers/Biography/index.js
+++ src/containers/Biography/index.js
@@ -1,20 +1,34 @@
 import React from 'react';
-import { classNames as cn } from 'peculiarity';
+import { safeGet, classNames as cn } from 'peculiarity';
+
+import { Link } from '../../components';
 
 import styles from './styles.module.scss';
 
 const Biography = ({ className, biography }) => {
-  const { name, biography: bio, experience } = biography;
-  const {
-    title,
-    organization: { name: organization },
-  } = experience;
+  const { name, biography: bio, experience } = safeGet(
+    biography,
+    'name',
+    'biography',
+    'experience',
+  );
+
+  const { title, name: organization, website } = safeGet(
+    experience,
+    'title',
+    ['organization', 'name'],
+    ['organization', 'website'],
+  );
 
   return (
     <section className={cn(styles.biography, className)}>
-      <h1 className={styles.name}>{name}</h1>
-      <span className={styles.title}>{title} at</span>
-      <h2 className={styles.organization}>{organization}</h2>
+      <div className={styles.grid}>
+        <h1 className={styles.name}>{name}</h1>
+        <span className={styles.title}>{title} at</span>
+        <h2 className={styles.organization}>
+          <Link to={website}>{organization}</Link>
+        </h2>
+      </div>
       <p className={styles.bio}>{bio}</p>
     </section>
   );
