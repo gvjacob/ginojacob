c9c00b731c39d331c996018807aeb8534514e197
gvjacob <gvjacob@outlook.com>
2020-04-20 13:29:17 -0400

    archive project media

--- src/components/FALink/index.js
+++ src/components/FALink/index.js
@@ -6,7 +6,7 @@ import Link from '../Link';
 import styles from './styles.module.scss';
 
 const FALink = ({ className, link, fontAwesomeClass }) => (
-  <Link className={cn(styles.faLink, className)} to={link} newTab>
+  <Link className={cn(styles.faLink, className)} to={link}>
     <FAIcon className={styles.icon} fontAwesomeClass={fontAwesomeClass} />
   </Link>
 );
