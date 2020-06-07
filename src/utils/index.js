e946b9d5c21dbb0a1d1f77c32c47bdc1385fc26d
gvjacob <gvjacob@outlook.com>
2020-06-06 21:34:45 -0400

    delete utils file

--- src/utils/index.js
+++
@@ -1,7 +0,0 @@
-import { get } from 'lodash';
-
-export const serializeEdges = withEdges =>
-  get(withEdges, 'edges', []).map(edge => edge.node || edge);
-
-export const addQueryParams = (link, params) =>
-  Object.entries(params).reduce((acc, [key, value]) => `${acc}&${key}=${value}`, `${link}?`);
