0724509dcd94aceb1b7a69d5390c69624b424859
gvjacob <gvjacob@outlook.com>
2020-09-09 22:29:16 -0400

    baseline

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -6,11 +6,18 @@ import './styles.scss';
 const Hero = ({ headline, name, title, description, image }) => {
   return (
     <section>
-      <div className="hero-topper">
-        <h1 className="hero-headline">{headline}</h1>
-        <div className="hero-bio">
-          <h2 className="hero-name">{name}</h2>
-          <Markdown source={title} />
+      <div className="hero__topper">
+        <div className="hero__headline"></div>
+        <div className="hero__bio">
+          <div>
+            <h2 className="hero__name">{name}</h2>
+          </div>
+          <div>
+            <Markdown className="hero__title" source={title} />
+          </div>
+          <div>
+            <h1 className="hero__headline-text">{headline}</h1>
+          </div>
         </div>
       </div>
       <p>{description}</p>
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -3,12 +3,13 @@ import React, { Fragment } from 'react';
 import Navigation from '../Navigation';
 
 import '../../styles/index.scss';
+import './styles.scss';
 
 const Layout = ({ children }) => {
   return (
     <Fragment>
       <Navigation />
-      <main>{children}</main>
+      <main className="layout__main">{children}</main>
     </Fragment>
   );
 };
