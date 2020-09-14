770d85090457cb991a285da8464151d0f90bdd1a
gvjacob <gvjacob@outlook.com>
2020-09-14 13:20:29 -0400

    footer copyright

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
+
 import css from './styles.module.scss';
 
-const Footer = () => {
-  return null;
+const Footer = ({ email, copyright }) => {
+  return (
+    <footer>
+      <span>{email}</span>
+      <div className={css.copyright}>
+        <span>{copyright}</span>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
