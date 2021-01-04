08575f4b8eefa15101de1b94bd7f6fc0dc1db9f2
gvjacob <gvjacob@outlook.com>
2021-01-03 20:16:51 -0500

    fonts

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { If } from 'react-if';
+import isEmpty from 'lodash.isempty';
 
 import Link from '@components/Link';
 
@@ -7,10 +8,10 @@ import styled from './styled';
 
 const Header = ({ className, links }) => (
   <styled.Header className={className}>
-    <If condition={links}>
+    <If condition={!isEmpty(links)}>
       <styled.HeaderLinks>
         {links.map(({ label, to }) => (
-          <li key={label}>
+          <li className="ff-sans-serif-body-sub" key={label}>
             <Link to={to}>{label}</Link>
           </li>
         ))}
