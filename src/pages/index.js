d0d35f7e2db77be1fcb4f5525f2a9935edf30da9
gvjacob <gvjacob@outlook.com>
2021-01-07 15:46:17 -0500

    pull featured projects for index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 import styled from 'styled-components';
 
 import Link from '@components/Link';
@@ -21,54 +22,67 @@ const StyledArchiveTease = styled(ArchiveTease)`
   margin: 100px 0;
 `;
 
-const IndexPage = () => (
-  <Layout>
-    <StyledHero
-      name="Gino Jacob"
-      photo="/assets/profile.jpg"
-      date="02.16.2020"
-      position={
-        <>
-          Currently leading a project at{' '}
-          <Link to="https://scout.camd.northeastern.edu/">Scout</Link>
-        </>
-      }
-    />
-    <StyledBiography
-      about="Lucky to have built a life-saving app, democratized technology skills, and developed some really pretty websites. I'm an engineer, and above all else, an unapologetically user-centric designer."
-      status="Currently in Boston. Making cool stuff at Upstatement. Learning (or really relearning) Spanish on Duolingo. Still quarantining 😷"
-    />
-    <FeaturedWork
-      featured={[
-        {
-          title: 'B612',
-          description:
-            'A simple and personal brand identity for a wellness experience service.',
-          link: '#',
-          cover: '/assets/b612.png',
-        },
-        {
-          title: 'Covid Protocols',
-          description:
-            'Definitive clinical guidelines for frontline workers during COVID-19.',
-          link: '#',
-          cover: '/assets/covid-protocols.png',
-        },
-      ]}
-    />
-    <StyledArchiveTease
-      categories={[
-        { name: 'Web Design', to: '#' },
-        { name: 'Front-end', to: '#' },
-        { name: 'Backend', to: '#' },
-        { name: 'Creative Direction', to: '#' },
-        { name: 'Branding', to: '#' },
-        { name: 'Logo', to: '#' },
-        { name: 'Open Source', to: '#' },
-        { name: 'Ballroom 💃', to: '#' },
-      ]}
-    />
-  </Layout>
-);
+const IndexPage = ({ data }) => {
+  const featuredProjects = data.featured.edges.map(
+    ({ node }) => node.frontmatter,
+  );
+
+  return (
+    <Layout>
+      <StyledHero
+        name="Gino Jacob"
+        photo="/assets/profile.jpg"
+        date="02.16.2020"
+        position={
+          <>
+            Currently leading a project at{' '}
+            <Link to="https://scout.camd.northeastern.edu/">Scout</Link>
+          </>
+        }
+      />
+      <StyledBiography
+        about="Lucky to have built a life-saving app, democratized technology skills, and developed some really pretty websites. I'm an engineer, and above all else, an unapologetically user-centric designer."
+        status="Currently in Boston. Making cool stuff at Upstatement. Learning (or really relearning) Spanish on Duolingo. Still quarantining 😷"
+      />
+      <FeaturedWork featured={featuredProjects} />
+      <StyledArchiveTease
+        categories={[
+          { name: 'Web Design', to: '#' },
+          { name: 'Front-end', to: '#' },
+          { name: 'Backend', to: '#' },
+          { name: 'Creative Direction', to: '#' },
+          { name: 'Branding', to: '#' },
+          { name: 'Logo', to: '#' },
+          { name: 'Open Source', to: '#' },
+          { name: 'Ballroom 💃', to: '#' },
+        ]}
+      />
+    </Layout>
+  );
+};
 
 export default IndexPage;
+
+export const query = graphql`
+  query FeaturedWorkQuery {
+    featured: allMarkdownRemark(
+      filter: {
+        fields: { sourceName: { eq: "projects" } }
+        frontmatter: { featured: { eq: true } }
+      }
+    ) {
+      edges {
+        node {
+          frontmatter {
+            title
+            tease
+            description
+            cover
+            categories
+            featured
+          }
+        }
+      }
+    }
+  }
+`;
