8e8d6bf2b04dbe45272d4025b9929440aef8e180
gvjacob <gvjacob@outlook.com>
2021-01-07 01:13:50 -0500

    simplify pages components

--- src/pages/archive.js
+++ src/pages/archive.js
@@ -1,4 +1,62 @@
+import React from 'react';
 import { graphql } from 'gatsby';
+import partition from 'lodash.partition';
+import styled from 'styled-components';
+
+import Topper from '@components/Topper';
+import Categories from '@components/Categories';
+import TeaseList from '@components/TeaseList';
+import Layout from '@components/Layout';
+import FeaturedWork from '@components/FeaturedWork';
+
+import { spacing } from '@styles/variables';
+
+const StyledTopper = styled(Topper)`
+  width: 50%;
+  margin-bottom: ${spacing.small};
+  margin-top: ${spacing.medium};
+`;
+
+const StyledCategories = styled(Categories)`
+  width: 66%;
+  margin-bottom: ${spacing.medium};
+`;
+
+const StyledTeaseList = styled(TeaseList)`
+  width: 50%;
+  margin-top: ${spacing.medium};
+  margin-bottom: ${spacing.large};
+`;
+
+const ArchivePage = ({ data }) => {
+  const projects = data.projects.edges.map(({ node }) => node.frontmatter);
+  const [featured, rest] = partition(projects, ({ featured }) => !!featured);
+
+  return (
+    <Layout>
+      <StyledTopper
+        title="Archive"
+        description="I’ve worked in the product and agency spaces. Over the years, I’ve had a hand in unique projects and taken multiple meaningful roles."
+      />
+      <StyledCategories
+        categories={[
+          { name: 'Web Design' },
+          { name: 'Front-end' },
+          { name: 'Backend' },
+          { name: 'Creative Direction' },
+          { name: 'Branding' },
+          { name: 'Logo' },
+          { name: 'Open Source' },
+          { name: 'Ballroom 💃' },
+        ]}
+      />
+      <FeaturedWork featured={featured} />
+      <StyledTeaseList projects={rest} />
+    </Layout>
+  );
+};
+
+export default ArchivePage;
 
 export const query = graphql`
   query ArchiveQuery {
@@ -20,5 +78,3 @@ export const query = graphql`
     }
   }
 `;
-
-export { default } from './_ArchivePage';
