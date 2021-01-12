017360a9377193f518815a726679865aea2e67b6
gvjacob <gvjacob@outlook.com>
2021-01-11 19:32:25 -0500

    responsive featured work

--- src/components/FeaturedWork/styled.js
+++ src/components/FeaturedWork/styled.js
@@ -1,10 +1,15 @@
 import styled from 'styled-components';
 import mixins from '@styles/mixins';
 import { spacing } from '@styles/variables';
+import { min } from '@styles/responsive';
 
 export default {
   Container: styled.section`
-    ${mixins.grid({ columns: '50% 40%', gap: '10%' })}
+    ${mixins.flex({ gap: spacing.large })}
+
+    @media ${min.tablet} {
+      ${mixins.grid({ columns: '50% 40%', gap: '10%' })}
+    }
   `,
 
   ProjectImage: styled.img`
