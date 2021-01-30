472563fd83d19561550635f1050b4c5fbcd5d28e
gvjacob <gvjacob@outlook.com>
2021-01-30 11:02:28 -0500

    Anchor links

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,13 +1,15 @@
-import "babel-polyfill";
+import 'babel-polyfill';
 
-import { onDocumentReady } from "./utils";
-import ExternalLinks from "./ExternalLinks";
-import HorizontalParallax from "./HorizontalParallax";
+import { onDocumentReady } from './utils';
+import Anchors from './Anchors';
+import ExternalLinks from './ExternalLinks';
+import HorizontalParallax from './HorizontalParallax';
 
 onDocumentReady(() => {
   new ExternalLinks();
+  new Anchors();
 
-  if (document.querySelector(".js-horizontal-parallax")) {
+  if (document.querySelector('.js-horizontal-parallax')) {
     new HorizontalParallax();
   }
 });
