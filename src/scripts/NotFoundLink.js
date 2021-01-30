21760eac4a45a44e9850950bfe327d249e47687d
gvjacob <gvjacob@outlook.com>
2021-01-30 17:08:24 -0500

    404 seo title

--- src/scripts/NotFoundLink.js
+++ src/scripts/NotFoundLink.js
@@ -1,3 +1,7 @@
+/**
+ * Update not found link on 404 page
+ * based on current page's pathname and search.
+ */
 class NotFoundLink {
   constructor() {
     const link = document.querySelector('.js-not-found-link');
